16edb6534b8100be872076a874ab78d105590030
Priscila Roza <priscilarozasilva@gmail.com>
2022-07-16 20:18:38 -0300

    Banner adicionado na home

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <template>
-  <Navbar :logo="logo_src" :alt="app_name"/>
+  <Navbar :logo="logo_src" :alt="app_name" />
   <router-view></router-view>
   <Footer />
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 export default {
   components: {
     Navbar,
-    Footer
+    Footer,
   },
   data() {
     return {
-      logo_src: 'img/logo.png',
-      app_name: "Make Your Burger"
-      }
-    }
-  }
+      logo_src: "img/logo.png",
+      app_name: "Make Your Burger",
+    };
+  },
+};
 </script>
 <style>
-  *{
-    font-family: Helvetica, sans-serif;
-    padding: 0;
-    border: 0;
-    box-sizing: border-box;
-  }
-  .main-container{
-    margin: 50px;
-    height: 250px;
-  }
-  h1{
-    text-align: center;
-    font-size: 42px;
-    margin-bottom: 30px;
-    color: #222;
-  }
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container {
+  margin: 50px;
+  height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
