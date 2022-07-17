610be4ba86ba723a890e42dfc9e8e244b41940a8
Priscila Roza <priscilarozasilva@gmail.com>
2022-07-17 19:47:57 -0300

    botão cancelar pedido funcionando

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -26,7 +26,7 @@
         <option>Selecione</option>
         <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">{{s.tipo}}</option>
         </select>
-        <button class="delete-btn">Cancelar</button>
+        <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
     </div>
     </div>
 </div>
@@ -66,10 +66,18 @@ export default {
             this.status = data;
             console.log(data)
         },
+        async deleteBurger(id) {
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: "DELETE"
+            });
+            const res = await req.json();
+            //msg
+            this.getPedidos();
+    },
     },
     mounted() {
         this.getPedidos();
-    }
+    },
     }
 </script>
 
